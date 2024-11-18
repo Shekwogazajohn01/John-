9a597cb35c96b6f8ba9b10b37cfc05e2c56ec9d7
Shekwogazajohn01 <admanjjwrld@gmail.com>
2024-11-19 00:18:21 +0100

    Add files via upload

---
+++ john.py
@@ -0,0 +1,80 @@
+def calculate_addition():
+    a=int(input('a'))
+    b=int(input('b'))
+    c=int(input('c'))
+    addition=a+b+c
+
+
+
+
+
+    print('addition_is',addition)
+calculate_addition()
+
+
+
+
+
+def calculate_subtraction():
+    a=int(input('a'))
+    b=int(input('b'))
+    c=int(input('c'))
+    subtraction=a-b-c
+
+
+
+
+    print('subtraction_is',subtraction)
+calculate_subtraction()
+
+
+
+
+
+def calculate_multiplication():
+    a=float(input('a'))
+    c=float(input('c'))
+    d=float(input('d'))
+    multiplication=a*c*d
+
+
+
+
+    print('multiplication_is',multiplication)
+calculate_multiplication()
+
+
+
+
+
+def calculate_division():
+    b=int(input('b'))
+    d=int(input('d'))
+    e=int(input('e'))
+    division=b/d/e
+
+
+
+    print('division_is',division)
+calculate_division()
+
+
+
+
+
+
+def calculate_area_of_rectangle():
+    length=int(input('length'))
+    breadth=int(input('breadth'))
+    area_of_rectangle=length*breadth
+
+
+
+    print('area_of_rectangle',area_of_rectangle)
+calculate_area_of_rectangle()
+
+
+
+
+
+
